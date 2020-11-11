8797a004af933a296a294f7548865e9b50076000
Gaspar Lukacs <37374850+gasparl@users.noreply.github.com>
2020-11-11 11:08:03 +0100

    Add files via upload

---
+++ similar_text.py
@@ -0,0 +1,52 @@
+# from https://github.com/luosch/similar_text
+
+def similar_str(str1, str2):
+    """
+    return the len of longest string both in str1 and str2
+    and the positions in str1 and str2
+    """
+    max_len = tmp = pos1 = pos2 = 0
+    len1, len2 = len(str1), len(str2)
+
+    for p in range(len1):
+        for q in range(len2):
+            tmp = 0
+            while p + tmp < len1 and q + tmp < len2 \
+                    and str1[p + tmp] == str2[q + tmp]:
+                tmp += 1
+
+            if tmp > max_len:
+                max_len, pos1, pos2 = tmp, p, q
+
+    return max_len, pos1, pos2
+
+
+def similar_char(str1, str2):
+    """
+    return the total length of longest string both in str1 and str2
+    """
+    max_len, pos1, pos2 = similar_str(str1, str2)
+    total = max_len
+
+    if max_len != 0:
+        if pos1 and pos2:
+            total += similar_char(str1[:pos1], str2[:pos2])
+
+        if pos1 + max_len < len(str1) and pos2 + max_len < len(str2):
+            total += similar_char(str1[pos1 + max_len:], str2[pos2 + max_len:]);
+
+    return total
+
+
+def similar_text(str1, str2):
+    """
+    return a int value in [0, 100], which stands for match level
+    """
+    if not (isinstance(str1, str) or isinstance(str1, unicode)):
+        raise TypeError("must be str or unicode")
+    elif not (isinstance(str2, str) or isinstance(str2, unicode)):
+        raise TypeError("must be str or unicode")
+    elif len(str1) == 0 and len(str2) == 0:
+        return 0.0
+    else:
+        return int(similar_char(str1, str2) * 200.0 / (len(str1) + len(str2)))
